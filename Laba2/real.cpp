d503381fddceb8e0e1d86bf594389b0f24b3cb2f
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-02-20 21:49:36 +0200

    The read_file function has been released, all variables are in their arrays and are waiting for SOVa

--- Laba2/real.cpp
+++ Laba2/real.cpp
@@ -1,13 +1,14 @@
 #include "real.h"
 
 void read_file(string path, vector<string>& team, vector<int>& points, int& teamsCount) {
-	int point, t1, t2, index = 0;
+	int point = 0, t1, t2, index = 0;
 	string line, teamName, goals1, goals2;
 	ifstream in(path);
 	if (in.is_open()) {
 		getline(in, line);
 		teamsCount = stoi(line);
 		while (getline(in, line)) {
+			line += ',';
 			for (int i = 0; i < line.length(); i++) {
 				if (line[i] == ',') {
 					if (index == 0) {
@@ -20,10 +21,14 @@ void read_file(string path, vector<string>& team, vector<int>& points, int& team
 						goals2.assign(line, index + 3, 1);
 						t1 = stoi(goals1);
 						t2 = stoi(goals2);
+						if (t1 > t2)point += 3;
+						else if (t1 == t2)point++;
+						index = i;
 					}
 				}
 			}
-			index = 0;
+			points.push_back(point);
+			index = 0, point = 0;
 		}
 		in.close();
 	}
