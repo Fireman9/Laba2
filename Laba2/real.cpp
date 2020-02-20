6d8d94dd3d9a89f6d2fe9922f63c73e50367b6b1
Fireman9 <54212619+Fireman9@users.noreply.github.com>
2020-02-20 21:26:46 +0200

    Released getting variables from file

--- Laba2/real.cpp
+++ Laba2/real.cpp
@@ -1,8 +1,8 @@
 #include "real.h"
 
 void read_file(string path, vector<string>& team, vector<int>& points, int& teamsCount) {
-	int index = 0;
-	string line;
+	int point, t1, t2, index = 0;
+	string line, teamName, goals1, goals2;
 	ifstream in(path);
 	if (in.is_open()) {
 		getline(in, line);
@@ -10,9 +10,20 @@ void read_file(string path, vector<string>& team, vector<int>& points, int& team
 		while (getline(in, line)) {
 			for (int i = 0; i < line.length(); i++) {
 				if (line[i] == ',') {
-					
+					if (index == 0) {
+						teamName.assign(line, index, i - index);
+						team.push_back(teamName);
+						index = i;
+					}
+					else {
+						goals1.assign(line, index + 1, 1);
+						goals2.assign(line, index + 3, 1);
+						t1 = stoi(goals1);
+						t2 = stoi(goals2);
+					}
 				}
 			}
+			index = 0;
 		}
 		in.close();
 	}
