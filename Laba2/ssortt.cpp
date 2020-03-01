238edc8acb0a26fac02e2a7c42e002bb5d24f5fe
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-03-01 22:41:21 +0200

    Added a feature  to find the maximum element

--- Laba2/ssortt.cpp
+++ Laba2/ssortt.cpp
@@ -4,12 +4,27 @@
 
 using namespace std;
 
+int maxEl(vector<int> points, int a, int b);
+
 void ssort(vector<string> team, vector<int> points) {
 	int a = points.size();
-	int b, c;
+	int b, c, d;
 	for (int i = 0;  i < a;  i++)
 	{
+		
 	}
 }
 
-int maxEl(vector<int> points) 
+int maxEl(vector<int> points, int a, int b) {
+	int max = points[a];
+	int indexMax = a;
+	for (int i = a; i < b; i++)
+	{
+		if (max<points[i])
+		{
+			max = points[i];
+			indexMax = i;
+		}
+	}
+	return indexMax;
+}
--- Laba2/ssortt.h
+++ Laba2/ssortt.h
@@ -6,4 +6,4 @@
 using namespace std;
 
 void ssort(vector<string> team, vector<int> points);
-int maxEl(vector<int> points);
+int maxEl(vector<int> points, int a, int b);
