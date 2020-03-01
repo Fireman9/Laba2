e3fcb62741df5f270938911cabd6dfe474f237ed
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-03-01 21:47:06 +0200

    Added prototypes for sorting and finding the maximum element in a vector

---
+++ Laba2/ssortt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+void ssort(vector<string> team, vector<int> points) {
+	int a = points.size();
+	int b, c;
+	for (int i = 0;  i < a;  i++)
+	{
+	}
+}
+
+int maxEl(vector<int> points) 
---
+++ Laba2/ssortt.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+void ssort(vector<string> team, vector<int> points);
+int maxEl(vector<int> points);
